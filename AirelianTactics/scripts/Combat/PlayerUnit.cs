4d58aabc187fb677327a4986981f79b2a04be6e2
AurelianTactics <jamesdilorenzo@gmail.com>
2025-03-19 20:21:44 -0400

    basic playerunit

---
+++ AirelianTactics/scripts/Combat/PlayerUnit.cs
@@ -0,0 +1,67 @@
+public class PlayerUnit {
+
+    // stats can come from Unit or Item. Total is the sum of the two. In future maybe add more components   
+    private int StatTotalCT { get; set; }
+    private int StatTotalSpeed { get; set; }
+    private int StatTotalPA { get; set; }
+    private int StatTotalHP { get; set; }
+    private int StatTotalMove { get; set; }
+    private int StatTotalJump { get; set; }
+
+    private int UnitId { get; set; }
+
+    // if all units on a team are incapacitated, the team is defeated
+    private bool IsIncapacitated { get; set; }
+
+    public PlayerUnit(int ct, int speed, int pa, int hp, int move, int jump, int unitId) {
+        this.StatTotalCT = ct;
+        this.StatTotalSpeed = speed;
+        this.StatTotalPA = pa;
+        this.StatTotalHP = hp;
+        this.StatTotalMove = move;
+        this.StatTotalJump = jump;
+
+        this.UnitId = unitId;
+        this.IsIncapacitated = false;
+    }
+
+    public bool IsEligibleForActiveTurn() {
+        bool isEligible = !this.IsIncapacitated && this.StatTotalCT >= 100;
+
+        return isEligible;
+    }
+
+    public void AddCT()
+    {
+        //doing simple version for now until statusManager is implemented
+        if( this.StatTotalCT < 100)
+        {
+            this.StatTotalCT += this.StatTotalSpeed;
+
+            // to do: calculate CT more correctly
+            // if( !StatusManager.Instance.IsCTHalted(this.TurnOrder))
+            // {
+            //     int z1 = this.StatTotalSpeed;
+            //     if( IsAbilityEquipped(NameAll.SUPPORT_NATURAL_HIGH, NameAll.ABILITY_SLOT_SUPPORT) && StatusManager.Instance.IsPositiveStatus(this.TurnOrder))
+            //     {
+            //         z1 += 1;
+            //     }
+            //     else if ( IsAbilityEquipped(NameAll.SUPPORT_UNNATURAL_HIGH, NameAll.ABILITY_SLOT_SUPPORT) && StatusManager.Instance.IsNegativeStatus(this.TurnOrder))
+            //     {
+            //         z1 += 2;
+            //     }
+            //     z1 = StatusManager.Instance.ModifySpeed(this.TurnOrder, z1);
+            //     this.CT += z1;
+            // }  
+        }
+    }
+
+    public void AlterHP(int hp) {
+        this.StatTotalHP += hp;
+
+        if (this.StatTotalHP <= 0) {
+            this.IsIncapacitated = true;
+            this.StatTotalHP = 0;
+        }
+    }
+}
